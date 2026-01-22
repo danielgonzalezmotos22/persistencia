c32ecc712c81ba13df1bb1773a973a6418edc1bb
danielgonzalezmotos22 <danielgonzalezmotos22@gmail.com>
2026-01-22 12:26:19 +0100

    test

---
+++ test_persistance/test_persistencia.py
@@ -0,0 +1,40 @@
+from persistencia import persistencia
+
+def test_archivo_encontrado():
+    #el archivo se ha encontrado correctamente
+    pass
+
+def test_archivo_leido_correctamente():
+    #se leen todos los caracteres
+    pass
+
+def test_archivo_escrito_correctamente():
+    #se escribe los parametros que le hemos pedido
+    pass
+
+def test_no_cambio_de_valores():
+    #si el archivo no ha cambieado el calor por clave y viceversa
+    pass
+
+def test_archivo_no_escriba_debajo():
+    #no escriba debajo de otro
+    pass
+
+def test_archivo_actualizado():
+    #si se ha actualizado correctamente
+    pass
+
+def test_archivo_no_sobreescrito():
+    #si en vez de actualizarlo se ha sobreescrito todo
+    pass
+
+def test_archivo_limpiado():
+    #el archivo se ha limpiado bien
+    pass
+
+def test_archivo_no_eliminado_con_clear():
+    pass
+
+def test_archivo_borrado():
+    #el archivo se ha borrado correctamente
+    pass
